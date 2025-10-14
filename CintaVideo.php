5a19e7132ea71a2c648163ea973615afd1282f75
Marc <maralcban@alu.edu.gva.es>
2025-10-14 16:50:59 +0200

    CintaVideo.php Acabado

--- CintaVideo.php
+++ CintaVideo.php
@@ -1,17 +1,20 @@
 <?php
 require_once 'Soporte.php';
+
 class CintaVideo extends Soporte {
     private $duracion;
 
-    public function __construct($titulo,$numero, $precio, $duracion) 
+    public function __construct($titulo, $numero, $precio, $duracion) 
     {
-        parent::__construct($titulo,$numero,$precio);
+        parent::__construct($titulo, $numero, $precio);
         $this->duracion = $duracion;
     }
 
     public function muestraResumen() {
-
+        echo "Película en VHS:\n";
+        echo "<br>$this->titulo\n";
+        echo "<br>".$this->getPrecio() . " € (IVA no incluido)\n";
+        echo "<br>Duración: $this->duracion minutos\n";
     }
 }
-
 ?>
